8910ea7dbda77bb14291eb37bc525744c34e1ac4
JsNox <joel.silos@hotmail.com>
2025-03-08 23:11:32 -0600

    componentes calorieTracker y CalorieDisplay

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useEffect, useMemo, useReducer } from "react"
 import Form from "./components/Form"
 import { activityReducer, initialState } from "./reducers/activity-reducer"
 import ActivityList from "./components/ActivityList"
+import CalorieTracker from "./components/CalorieTracker"
 
 
 function App() {
@@ -37,6 +38,16 @@ function App() {
           dispatch={dispatch}
           state={state}
         />
+
+      </section>
+
+      <section className="bg-gray-800 py-10">
+        <div className="max-w-4xl mx-auto">
+          <CalorieTracker 
+            activities={state.activities}
+          />
+
+        </div>
 
       </section>
 
